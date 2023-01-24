3cc6ecbba1856a7f9aa00cf5e1b4a4c4ec425620
timdenisiuk <59811685+timdenisiuk@users.noreply.github.com>
2023-01-24 11:37:35 -0800

    Test for mobile

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,10 @@ export default function Home() {
       </Head>
       <div className="box-border p-0 m-0 font-sans font-bold bg-blue-200">
         <main className="flex flex-col items-center min-h-screen p-8">
-          <h1 className="text-5xl mr-80 mb-14 pr-96 ">Contact us!</h1>
-          <p className="text-xl pb-28">Questions? Comments? Concerns? Send us an email, and we will get back to you as soon as possible!</p>
+          <h1 className="text-2xl text-center sm:text-center lg:text-5xl lg:pb-5">Contact us!</h1>
+          <p className="lg:text-xl">Questions? Comments? Concerns?</p>
+          <p className="lg:text-xl">Send us an email, and we will get back to you</p>
+          <p className="lg:text-xl lg:pb-28">as soon as possible!</p>
           <button className = "px-4 py-2 font-bold text-white bg-blue-500 border border-blue-700 rounded hover:bg-blue-700"
           onClick={() => setIsTriggered(true) }>Contact Us</button>
           <Form trigger={isTriggered} setTrigger={setIsTriggered} route={router}/>
