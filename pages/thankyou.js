6cc7e333856aab177cefc350f418768d5a6b27ae
timdenisiuk <59811685+timdenisiuk@users.noreply.github.com>
2023-01-24 22:15:46 -0800

    added merriweather font for all text

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -10,7 +10,7 @@ const ThankYou = () => {
                 <title>Thank You</title>
             </Head>
             <div className="box-border p-0 m-0 font-sans font-bold bg-blue-200">
-                <main className="flex flex-col items-center min-h-screen p-8">
+                <main className="flex flex-col items-center min-h-screen p-8 font-merriweather">
                     <h1 className="pb-4 text-2xl lg:text-6xl">Thank You!</h1>
                     <h1 className="text-lg lg:text-4xl">You should hear from us soon.</h1>
                     <Link href="/" className="pt-12 text-2xl text-blue-600 underline">Home</Link>
